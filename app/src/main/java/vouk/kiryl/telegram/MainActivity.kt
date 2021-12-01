1d4f23274acf575b0190a9d229f01a22f6304461
Kirill Vovk <kirill.vovk.2002@mail.ru>
2021-12-02 00:53:57 +0300

    add: header & drawer

--- app/src/main/java/vouk/kiryl/telegram/MainActivity.kt
+++ app/src/main/java/vouk/kiryl/telegram/MainActivity.kt
@@ -2,10 +2,65 @@ package vouk.kiryl.telegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import vouk.kiryl.telegram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawer: Drawer
+    private lateinit var header: AccountHeader
+    private lateinit var toolBar: androidx.appcompat.widget.Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(toolBar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        drawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(toolBar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(header)
+            .addDrawerItems(
+                ProfileDrawerItem()
+                    .withIdentifier(100)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun createHeader() {
+        header = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem()
+                    .withName("Kirill Vovk")
+                    .withEmail("kirill@mail.com")
+            ).build()
+    }
+
+    private fun initFields() {
+        toolBar = binding.mainToolbar
     }
 }
